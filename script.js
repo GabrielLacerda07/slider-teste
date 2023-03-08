a5f2393726b993269c172d9d04ffbc4c41db021b
gabriellacerda07 <gabriellacerdacosta07@gmail.com>
2023-03-08 19:59:40 -0300

    Adc func de navegar entre imgs

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const carousel = document.querySelector('.carousel')
+firstImg = carousel.querySelectorAll('img')[0]
+let arrowIcons = document.querySelectorAll('span')
+let arrArrowIcons = Array.from(arrowIcons)
+
 let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = 250
+console.log(firstImgWidth)
+arrArrowIcons.forEach(icon=>{
+    icon.addEventListener('click', ()=>{
+        if(icon.id == 'left'){
+            carousel.scrollLeft -= firstImgWidth
+        }else{
+            carousel.scrollLeft += firstImgWidth
+        }
+    })
+})
+
 
 
 carousel.addEventListener('mousedown', (e)=>{
